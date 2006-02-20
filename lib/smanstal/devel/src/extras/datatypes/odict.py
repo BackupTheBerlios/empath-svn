8ad1186601c30f33a58d6299623a63da4ca7e876
smokybobo <smokybobo@2d1e70a7-801d-0410-ab83-90da37f5c1c7>
2006-02-20 19:26:21 +0000

    Added code for copying of functions and methods * Converted original code to use RuleDispatch's dispatch module

--- lib/smanstal/devel/src/extras/datatypes/odict.py
+++ lib/smanstal/devel/src/extras/datatypes/odict.py
@@ -1,6 +1,6 @@
 ###########################################################################
 # extras.datatypes.odict -- Ordered dictionary
-# Copyright (C) 2004  Ariel De Ocampo
+# Copyright (C) 2006  Ariel De Ocampo
 # 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
