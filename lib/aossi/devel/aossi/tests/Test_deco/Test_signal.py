c6c252f75191465d78d61d27c287a0421dfbda9f
smokybobo <smokybobo@2d1e70a7-801d-0410-ab83-90da37f5c1c7>
2006-06-14 19:22:52 +0000

    Implement signal decorator.
    context: aossi.deco
    
    aossi.deco.signal is a decorator that wraps a decoratable callable
    in a DecoSignal instance, which in turn contains decorator
    methods for further attaching other methods/functions.
    
    These DecoSignal decorator methods only do connection and do not
    in any way modify the original callable.
    
    Unit tests: aossi.tests.Test_deco
    
    Other changes
    -------------
    Added support for 'weak' keyword to aossi.signal.Signal class. If
    this is set to False, uses a hard reference to the passed in
    callable.
    
    Added support for 'weakcondf' keyword to aossi.signal.Signal class. If
    this is set to False, uses a hard reference for all conditional functions
    sent as part of the 'choose' keyword argument.

---
+++ lib/aossi/devel/aossi/tests/Test_deco/Test_signal.py
@@ -0,0 +1,79 @@
+# Module: aossi.tests.Test_deco.Test_signal
+# File: Test_signal.py
+# Copyright (C) 2006 Ariel De Ocampo arieldeocampo@gmail.com
+#
+# This module is part of the aossi project and is released under
+# the MIT License: http://www.opensource.org/licenses/mit-license.php
+
+import unittest
+from aossi.deco import signal, DecoSignal
+from aossi.cwrapper import CallableWrapper, cid
+from inspect import isfunction, ismethod
+
+class Testsignal(unittest.TestCase): #{{{
+    def setUp(self): #{{{
+        pass
+    # End def #}}}
+
+    def tearDown(self): #{{{
+        pass
+    # End def #}}}
+
+    def _basictest(self, s, ctype=isfunction): #{{{
+        self.assert_(ctype(s))
+        self.assert_(hasattr(s, 'signal'))
+        self.assert_(hasattr(s, 'settings'))
+        self.assert_(hasattr(s, 'global_settings'))
+        self.assert_(hasattr(s, 'after'))
+        self.assert_(hasattr(s, 'before'))
+        self.assert_(hasattr(s, 'around'))
+        self.assert_(hasattr(s, 'onreturn'))
+        self.assert_(hasattr(s, 'cond'))
+        self.assert_(hasattr(s, 'when'))
+        self.assert_(isinstance(s.signal, DecoSignal))
+        self.assertEqual(s(), 1)
+    # End def #}}}
+
+    def testReturnValue(self): #{{{
+        '''Returns a properly decorated function'''
+        def DummyFunction(): #{{{
+            return 1
+        # End def #}}}
+        s = signal(DummyFunction)
+        self._basictest(s)
+    # End def #}}}
+
+    def testFunctionDecorator(self): #{{{
+        '''Decorating a function'''
+        @signal
+        def DummyFunction(): #{{{
+            return 1
+        # End def #}}}
+
+        s = DummyFunction
+        self._basictest(s)
+    # End def #}}}
+
+    def testMethodDecorator(self): #{{{
+        '''Decorating a method'''
+        class A(object): #{{{
+            @signal
+            def a(self): #{{{
+                return 1
+            # End def #}}}
+        # End class #}}}
+
+        s = A().a
+        self._basictest(s, ismethod)
+    # End def #}}}
+
+    def testOneSignal(self): #{{{
+        '''A callable can only be decorated once as a signal'''
+        def testme():
+            pass
+        s1 = signal(testme)
+        s2 = signal(s1)
+        self.assert_(s1 is s2)
+    # End def #}}}
+# End class #}}}
+
