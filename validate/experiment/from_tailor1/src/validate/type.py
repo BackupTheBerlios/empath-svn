2a8b18f7e51dac4f79557d500bc85908e3995c53
smokybobo <smokybobo@2d1e70a7-801d-0410-ab83-90da37f5c1c7>
2006-09-18 21:26:22 +0000

    [validate @ bf867bbf59c28c89500672748df95865c147de3d] Setup basic modules under the validate package
    There are three modules: validate, type, value.
    
    validate - Contains all the base classes and/or functions required for
               the other modules. As of this commit, contains only the
               Validate base class that classes in the other two modules
               inherit from.
    
    type - Contains the ValidateType class, which, when instantiated, will
           validate a type against any object passed into it or compared
           against it.
    
    value - Contains the ValidateValue class. When instantiated will validate
            a value against any object passed into it or compared against it.
    
    Original author: "Ariel De Ocampo <arieldeocampo@gmail.com>"
    Date: 2006-09-03 12:33:37-04:00

---
+++ validate/experiment/from_tailor1/src/validate/type.py
@@ -0,0 +1,14 @@
+# Module: validate.type
+# File: type.py
+# Copyright (C) 2006 Ariel De Ocampo arieldeocampo@gmail.com
+#
+# This module is part of the validate project and is released under
+# the MIT License: http://www.opensource.org/licenses/mit-license.php
+
+from validate.validate import Validate
+
+__all__ = ('ValidateType',)
+
+class ValdateType(Validate): #{{{
+    pass
+# End class #}}}
