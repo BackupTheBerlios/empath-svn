2dda5e918d0a670f6c9233a9e4210884a7eb596a
smokybobo <smokybobo@2d1e70a7-801d-0410-ab83-90da37f5c1c7>
2006-09-18 22:09:26 +0000

    Commit add directory '/home/smokybobo/usr/local/repos/programming/hg/tailor/aossi/aossi/svn/aossi'

---
+++ aossi/devel/aossi/tests/__init__.py
@@ -0,0 +1,7 @@
+# Module: aossi.tests
+# File: __init__.py
+# Copyright (C) 2006 Ariel De Ocampo arieldeocampo@gmail.com
+#
+# This module is part of the aossi project and is released under
+# the MIT License: http://www.opensource.org/licenses/mit-license.php
+
