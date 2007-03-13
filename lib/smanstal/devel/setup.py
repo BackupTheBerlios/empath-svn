cc79d512fefb25e31e1c5b0342511031a27affa0
smokybobo <smokybobo@2d1e70a7-801d-0410-ab83-90da37f5c1c7>
2007-03-13 21:04:57 +0000

    Create default setup.py,MANIFEST.in for egg creation.

---
+++ lib/smanstal/devel/setup.py
@@ -0,0 +1,27 @@
+# Module: template
+# File: template.py
+# Copyright (C) 2006 Ariel De Ocampo arieldeocampo@gmail.com
+#
+# This module is part of the <WHAT-HAVE-YOU> project and is released under
+# the MIT License: http://www.opensource.org/licenses/mit-license.php
+from setuptools import setup, find_packages
+
+setup(
+  name="smanstal",
+  version="0.1.0",
+  description="""A collection of python utility functions and objects""",
+  author="Ariel De Ocampo",
+  author_email = 'arieldeocampo@gmail.com',
+
+
+#  entry_points="""
+#  [entry.point.name]
+#  ext-name = __name__.somemod:SomeCallable
+#  """
+
+#  package_data={'testme': 'data/somefile.dat'},
+
+  packages=find_packages('src'),
+  package_dir={'': 'src'}
+)
+
