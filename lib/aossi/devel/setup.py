d80a041555dc020987735b9cebd6dfc7e51b42ca
smokybobo <smokybobo@2d1e70a7-801d-0410-ab83-90da37f5c1c7>
2007-03-13 20:37:36 +0000

    Prepare project to turn into a python egg.
    * Move src/aossi/tests/ to src/aossi_tests/
      - This is in keeping with a policy to keep egg components
        nicely separated.
    * Create initial setup.py file.

---
+++ lib/aossi/devel/setup.py
@@ -0,0 +1,23 @@
+# Module: template
+# File: template.py
+# Copyright (C) 2006 Ariel De Ocampo arieldeocampo@gmail.com
+#
+# This module is part of the <WHAT-HAVE-YOU> project and is released under
+# the MIT License: http://www.opensource.org/licenses/mit-license.php
+from setuptools import setup, find_packages
+
+setup(
+    name="aossi",
+    version="0.1.0",
+    packages=find_packages('src'),
+    package_dir={'': 'src'}
+
+#    package_data={'aossi_docs': 'data/somefile.dat'},
+
+    # Metadata
+    description="""Signal-slot implementation""",
+    author="Ariel De Ocampo",
+    author_email = 'arieldeocampo@gmail.com',
+    license = "MIT"
+)
+
