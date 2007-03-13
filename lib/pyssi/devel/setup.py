1ede213bb95a5b8f34bee7902eb7f870c054315a
smokybobo <smokybobo@2d1e70a7-801d-0410-ab83-90da37f5c1c7>
2007-03-13 20:15:50 +0000

    Redo structure for creating python egg distributions.
    The directory structure is now as follows:
    
    src/
       All packages, modules, and resources are placed here
       as python packages i.e. a directory containing a file
       named '__init__.py'. Any directory with a name containing
       the string '__name__' should have that string replaced with
       the name of the python package.
    
       __name__/
          The main python package.
    
       __name___tests/
          All tests go in this directory.
    
          all/
             Runs all tests.
    
          doc/
             Runs doc tests.
    
          unit/
             Contains and runs unit tests.
    
             data/
                Any data required by the unit tests.
    
       __name___docs/
          Contains all documentation.
    
          api/
             Contains api documentation, usually as reStructuredText
             files.
    
    util/
       Any utility programs dealing with egg management.
    
       python/
          For python scripts.

---
+++ lib/pyssi/devel/setup.py
@@ -0,0 +1,27 @@
+# Module: template
+# File: template.py
+# Copyright (C) 2006 Ariel De Ocampo arieldeocampo@gmail.com
+#
+# This module is part of the <WHAT-HAVE-YOU> project and is released under
+# the MIT License: http://www.opensource.org/licenses/mit-license.php
+from setuptools import setup, find_packages
+
+setup(
+  name="SkeletonEgg",
+  version="0.0.1",
+  description="""SkeletonEgg""",
+  author="Ariel De Ocampo",
+  author_email = 'arieldeocampo@gmail.com',
+
+
+#  entry_points="""
+#  [entry.point.name]
+#  ext-name = __name__.somemod:SomeCallable
+#  """
+
+#  package_data={'testme': 'data/somefile.dat'},
+
+  packages=find_packages('src'),
+  package_dir={'': 'src'}
+)
+
