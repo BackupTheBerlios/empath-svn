7332c0c62dfc35bea3a838a650b4fbadf891ea2b
smokybobo <smokybobo@2d1e70a7-801d-0410-ab83-90da37f5c1c7>
2007-03-17 22:37:22 +0000

    Update smanstal.tests.* functions to be able to deal with non-path modules.
    Also create unit tests that run doc tests found in the api documentation.

--- lib/smanstal/devel/src/smanstal/tests/__init__.py
+++ lib/smanstal/devel/src/smanstal/tests/__init__.py
@@ -11,7 +11,8 @@ import doctest
 import os, os.path as op, re, sys
 
 from smanstal.types import (absmodpath, modpathmod, pathmod, hasinit, 
-        ispackage, isfunction, iscallable, isclass, isobjclass, isiterable)
+        ispackage, isfunction, iscallable, isclass, isobjclass, isiterable,
+        fromfile, isfindable)
 from smanstal.util.py import iff
 
 __all__ = ('testmodules', 'testsuites', 'alltestnames', 'alltestobjects', 'addtest', 'BaseUnitTest')
@@ -23,15 +24,15 @@ _DefaultDocTestRegex = re.compile(r'[Dd]oc[Tt]est')
 # =================================================
 # testmodules
 # =================================================
-def testmodules(suite, regex=None): #{{{
+def testmodules(suite, regex=None, filenames=False): #{{{
     if not regex:
         regex = _DefaultModNameRegex
     if ispackage(suite) or (isinstance(suite, basestring) and op.isdir(suite)):
-        return _tm_gen(suite, regex)
+        return _tm_gen(suite, regex, filenames)
     raise TypeError('Cannot walk through sub modules of %s object' %suite.__class__.__name__)
 # End def #}}}
 
-def _tm_gen(suite, regex): #{{{
+def _tm_gen(suite, regex, filenames): #{{{
     if not isinstance(regex, _REType):
         raise TypeError("%s object is not a compiled regular expression" %regex.__class__.__name__)
     topdir = suite
@@ -41,21 +42,22 @@ def _tm_gen(suite, regex): #{{{
         for f in files:
             name, ext = op.splitext(f)
             if ext == '.py' and regex.match(name):
-                yield absmodpath(op.join(topdir, f))
+                path = op.join(topdir, f)
+                yield (path if filenames else absmodpath(path))
         break
 # End def #}}}
 # =================================================
 # testsuites
 # =================================================
-def testsuites(suite, regex=None): #{{{
+def testsuites(suite, regex=None, filenames=False): #{{{
     if not regex:
         regex = _DefaultModNameRegex
     if ispackage(suite) or (isinstance(suite, basestring) and op.isdir(suite)):
-        return _ts_gen(suite, regex)
+        return _ts_gen(suite, regex, filenames)
     raise TypeError('Cannot walk through sub packages of %s object' %suite.__class__.__name__)
 # End def #}}}
 
-def _ts_gen(suite, regex): #{{{
+def _ts_gen(suite, regex, filenames): #{{{
     if not isinstance(regex, _REType):
         raise TypeError("%s object is not a compiled regular expression" %regex.__class__.__name__)
     topdir = suite
@@ -63,22 +65,23 @@ def _ts_gen(suite, regex): #{{{
         topdir = op.dirname(suite.__file__)
     for dir, subdir, files in os.walk(topdir):
         for d in subdir:
-            if hasinit(op.join(topdir, d)) and regex.match(d):
-                yield absmodpath(op.join(topdir, d))
+            path = op.join(topdir, d)
+            if hasinit(path) and regex.match(d):
+                yield (path if filenames else absmodpath(path))
         break
 # End def #}}}
 # =================================================
 # alltestnames
 # =================================================
-def alltestnames(suite, regex=None): #{{{
+def alltestnames(suite, regex=None, filenames=False): #{{{
     if not regex:
         regex = _DefaultModNameRegex
     if ispackage(suite) or (isinstance(suite, basestring) and op.isdir(suite)):
-        return _atn_gen(suite, regex)
+        return _atn_gen(suite, regex, filenames)
     raise TypeError('Cannot walk through sub packages/modules of %s object' %suite.__class__.__name__)
 # End def #}}}
 
-def _atn_gen(suite, regex): #{{{
+def _atn_gen(suite, regex, filenames): #{{{
     if not isinstance(regex, _REType):
         raise TypeError("%s object is not a compiled regular expression" %regex.__class__.__name__)
     topdir = suite
@@ -88,28 +91,33 @@ def _atn_gen(suite, regex): #{{{
         for f in files:
             name, ext = op.splitext(f)
             if ext == '.py' and regex.match(name):
-                yield absmodpath(op.join(topdir, f))
+                path = op.join(topdir, f)
+                yield path if filenames else absmodpath(path)
         for d in subdir:
-            if hasinit(op.join(topdir, d)) and regex.match(d):
-                yield absmodpath(op.join(topdir, d))
+            path = op.join(topdir, d)
+            if hasinit(path) and regex.match(d):
+                yield path if filenames else absmodpath(path)
         break
 # End def #}}}
 # =================================================
 # alltestobjects
 # =================================================
-def alltestobjects(suite, regex=None): #{{{
+def alltestobjects(suite, regex=None, filenames=False): #{{{
     if not regex:
         regex = _DefaultModNameRegex
     if ispackage(suite) or (isinstance(suite, basestring) and op.isdir(suite)):
-        return _ato_gen(suite, regex)
+        return _ato_gen(suite, regex, filenames)
     raise TypeError("%s object is not a package" %suite.__class__.__name__)
 # End def #}}}
 
-def _ato_gen(suite, regex): #{{{
+def _ato_gen(suite, regex, filenames): #{{{
     if not isinstance(regex, _REType):
         raise TypeError("%s object is not a compiled regular expression" %regex.__class__.__name__)
-    for modname in alltestnames(suite, regex):
-        mod = modpathmod(modname)
+    for modname in alltestnames(suite, regex, filenames):
+        if filenames:
+            mod = fromfile(modname)
+        else:
+            mod = modpathmod(modname)
         sfunc = getattr(mod, 'suite', None)
         if not sfunc or not iscallable(sfunc):
             raise ValueError("Test module '%s' is missing a suite callable" %modname)
@@ -160,12 +168,13 @@ def _addtest_testcase(testcase): #{{{
 # End def #}}}
 
 def _addtest_str(suite): #{{{
+    suite = op.abspath(suite)
     def deco(func): #{{{
         def wrapper(): #{{{
             test = TestSuite()
-            selfmod = pathmod(suite)
-#            print [type(t) for t in alltestobjects(selfmod)]
-            test.addTests(sfunc() for sfunc in alltestobjects(selfmod))
+            insys = isfindable(suite)
+            selfmod = pathmod(suite) if insys else fromfile(suite)
+            test.addTests(sfunc() for sfunc in alltestobjects(selfmod, filenames=(not insys)))
             ret = func()
             if isinstance(ret, TestCase) and ret not in test:
                 test.addTest(ret)
@@ -228,7 +237,8 @@ class BaseUnitTest(TestCase): #{{{
 # =================================================
 def mksuite(magicfile, ignore=None): #{{{
     def suite(): #{{{
-        curmod = pathmod(magicfile)
+        mf = op.abspath(magicfile)
+        curmod = pathmod(mf) if isfindable(mf) else fromfile(mf)
         test = TestSuite()
         count = 0
         for attr in dir(curmod):
---
+++ lib/smanstal/devel/tests/unit/TestMod_types/TestMod_callobj/TestFunc_quote/__init__.py
@@ -0,0 +1,16 @@
+# Module: anyall.tests.TestPkg_anyall.TestMod_callobj.TestFunc_quote
+# File: __init__.py
+# Copyright (C) 2006 Ariel De Ocampo arieldeocampo@gmail.com
+#
+# This module is part of the anyall project and is released under
+# the MIT License: http://www.opensource.org/licenses/mit-license.php
+
+import unittest
+from smanstal.tests import addtest, mksuite
+
+# Create suite function for this package
+suite = addtest(__file__)(mksuite(__file__))
+
+if __name__ == '__main__':
+    unittest.TextTestRunner(verbosity=2).run(suite())
+
