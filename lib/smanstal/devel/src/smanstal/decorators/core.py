6c2a229aee7d17ffd2df9a27e357e55a10e18290
smokybobo <smokybobo@2d1e70a7-801d-0410-ab83-90da37f5c1c7>
2007-03-18 01:38:12 +0000

    Removed useless function smanstal.decorators.addall

--- lib/smanstal/devel/src/smanstal/decorators/core.py
+++ lib/smanstal/devel/src/smanstal/decorators/core.py
@@ -5,7 +5,7 @@
 # This module is part of the <WHAT-HAVE-YOU> project and is released under
 # the MIT License: http://www.opensource.org/licenses/mit-license.php
 
-__all__ = ('property_', 'addall')
+__all__ = ('property_',)
 
 # Based on the following recipe:
 # http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/205183
@@ -13,14 +13,4 @@ def property_(func): #{{{
     vals = dict((k, v) for k,v in func().iteritems() 
             if k in ('fdel', 'fset', 'fget', 'doc'))
     return property(**vals)
-# End def #}}}
-
-def addall(all_list): #{{{
-    def wrap(func): #{{{
-        if not isinstance(all_list, list):
-            raise TypeError("Only lists are supported for __all__ with the addall decorator")
-        all_list.append(func.__name__)
-        return func
-    # End def #}}}
-    return wrap
 # End def #}}}
