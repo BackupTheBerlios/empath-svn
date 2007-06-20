cb5f3e7fa19f68a144229192519012624b6164fa
smokybobo <smokybobo@2d1e70a7-801d-0410-ab83-90da37f5c1c7>
2007-06-20 00:05:48 +0000

    Delete everything to prepare for a new multi-project layout.
    As there were many mistakes done in this repository, need to
    start "fresh" so to speak. As well, the code in this repository
    is too old, so will start uploading updated versions.
    
    The new layout has projects categorized by use.
    
    e.g.
    
    /
       lib/
          smanstal/
             devel/
       apps/

--- aossi/devel/src/aossi/tests/Test_cwrapper/Test_CallableWrapper/__init__.py
+++
@@ -1,7 +0,0 @@
-# Module: aossi.tests.sigslot.CallableWrapper
-# File: __init__.py
-# Copyright (C) 2006 Ariel De Ocampo arieldeocampo@gmail.com
-#
-# This module is part of the aossi project and is released under
-# the MIT License: http://www.opensource.org/licenses/mit-license.php
-
--- validate/dev/src/validate/tests/TestMod_type/TestClass_ValidateTypeMapping_And/__init__.py
+++
@@ -1,7 +0,0 @@
-# Module: template
-# File: template.py
-# Copyright (C) 2006 Ariel De Ocampo arieldeocampo@gmail.com
-#
-# This module is part of the <WHAT-HAVE-YOU> project and is released under
-# the MIT License: http://www.opensource.org/licenses/mit-license.php
-
