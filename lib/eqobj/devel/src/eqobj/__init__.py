fc456d64339cc35a9141d074517eaeb4271305e4
smokybobo <smokybobo@2d1e70a7-801d-0410-ab83-90da37f5c1c7>
2007-08-16 12:51:15 +0000

    First implementation of base abstract class EqObj.
    This is a very simple class that provides mechanisms to compare or
    validate a python object.
    
    It supports three ways to perform a comparison:
    
    1. bool casting of an EqObj object
       - If no object was specified as a base object when instantiating
         an EqObj object, the None object is used by default
    2. Calling an EqObj optionally passing an object to compare/validate
    3. Directly using the == operator
    
    A mixin or subclass would typically override __transform__ or __compare__
    methods. When doing a comparison/validation, the __eq__ method will pass
    its argument to transform whose return value is then passed to __compare__.
    The return value of the __compare__ method is what is returned.

--- lib/eqobj/devel/src/eqobj/__init__.py
+++ lib/eqobj/devel/src/eqobj/__init__.py
@@ -2,6 +2,11 @@
 # File: template.py
 # Copyright (C) 2006 Ariel De Ocampo arieldeocampo@gmail.com
 #
-# This module is part of the <WHAT-HAVE-YOU> project and is released under
+# This module is part of the eqobj project and is released under
 # the MIT License: http://www.opensource.org/licenses/mit-license.php
 
+from eqobj.core import __all__ as _core_all
+from eqobj.core import *
+
+all = list(_core_all)
+del _core_all
---
+++ lib/eqobj/devel/src/eqobj/core.py
@@ -0,0 +1,42 @@
+# Module: eqobj.core
+# File: core.py
+# Copyright (C) 2007 Ariel De Ocampo arieldeocampo@gmail.com
+#
+# This module is part of the eqobj project and is released under
+# the MIT License: http://www.opensource.org/licenses/mit-license.php
+
+__all__ = ('EqObj',)
+
+class EqObj(object): #{{{
+    __slots__ = ('_EqObj__initobj',)
+
+    def __init__(self, obj=None): #{{{
+        self.__initobj = obj
+    # End def #}}}
+
+    def __transform__(self, obj): #{{{
+        return obj
+    # End def #}}}
+
+    def __compare__(self, obj): #{{{
+        raise NotImplementedError
+    # End def #}}}
+
+    def __call__(self, *args): #{{{
+        obj = self.__initobj
+        if len(args) > 1:
+            raise TypeError("EqObj callables only accepts one argument")
+        if args:
+            obj = args[0]
+        return self.__eq__(obj)
+    # End def #}}}
+
+    def __eq__(self, obj): #{{{
+        obj = self.__transform__(obj)
+        return self.__compare__(obj)
+    # End def #}}}
+
+    def __nonzero__(self): #{{{
+        return self()
+    # End def #}}}
+# End class #}}}
