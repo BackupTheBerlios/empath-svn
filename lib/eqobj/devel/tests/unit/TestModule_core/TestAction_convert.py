5c0efbb0890433c9495fb6bb4b3195dd5dfa2fbb
smokybobo <smokybobo@2d1e70a7-801d-0410-ab83-90da37f5c1c7>
2007-08-16 13:21:46 +0000

    Add unit tests to test the __transform__ EqObj method

---
+++ lib/eqobj/devel/tests/unit/TestModule_core/TestAction_convert.py
@@ -0,0 +1,45 @@
+# Module: template
+# File: template.py
+# Copyright (C) 2007 Ariel De Ocampo arieldeocampo@gmail.com
+#
+# This module is part of the ${__name__} project and is released under
+# the MIT License: http://www.opensource.org/licenses/mit-license.php
+
+import unittest, re
+from smanstal.tests import BaseUnitTest, addtest, mksuite
+
+from eqobj.core import EqObj
+
+class Test_convert(BaseUnitTest): #{{{
+    def setUp(self): #{{{
+        class make_string(EqObj): #{{{
+            def __transform__(self, obj): #{{{
+                return str(obj).lower()
+            # End def #}}}
+            def __compare__(self, obj): #{{{
+                return self._initobj == obj
+            # End def #}}}
+        # End class #}}}
+        self.ms = make_string
+    # End def #}}}
+
+    def tearDown(self): #{{{
+        pass
+    # End def #}}}
+
+    def test_convert_method(self): #{{{
+        '''__transform__ method gets called'''
+        a = self.ms("hello")
+        for val in (1, 1.5, [], {}, (), "me", None, "", (1,)):
+            self.assertFalse(a(val))
+        for val in ("hello", "HELLO", "hElLo"):
+            self.assertTrue(a(val))
+    # End def #}}}
+# End class #}}}
+
+# Create suite function for this module
+suite = addtest(mksuite(__file__))
+
+if __name__ == '__main__':
+    unittest.TextTestRunner(verbosity=2).run(suite())
+
