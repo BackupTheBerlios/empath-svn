9d69d0d6a706b24d581665ebe11e3cff23e47170
smokybobo <smokybobo@2d1e70a7-801d-0410-ab83-90da37f5c1c7>
2007-08-19 21:26:42 +0000

    BUGFIX: eqobj.collections is not part of a unittest suite.
    Add base class CollectionMixin.

--- lib/eqobj/devel/src/eqobj/collections/__init__.py
+++ lib/eqobj/devel/src/eqobj/collections/__init__.py
@@ -5,12 +5,50 @@
 # This module is part of the eqobj project and is released under
 # the MIT License: http://www.opensource.org/licenses/mit-license.php
 
-import unittest
-from smanstal.tests import addtest, mksuite
+class CollectionMixin(object): #{{{
+    __slots__ = ()
+    def __init__(self, obj=(), **kwargs): #{{{
+        self._options = self._check_options(kwargs)
+        super(SequenceMixin, self).__init__(self.__transform__(obj))
+    # End def #}}}
 
-# Create suite function for this package
-suite = addtest(__file__)(mksuite(__file__))
+    def _check_options(self, opt, expected=()): #{{{
+        expected = frozenset(['count']) | frozenset(expected)
+        got = frozenset(opt)
+        if not expected.issuperset(got):
+            raise TypeError("Detected unknown keyword arguments: %s" %", ".join(got - expected))
+        return opt
+    # End def #}}}
 
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
+    def _pre_cmp(self, s, obj, target, options): #{{{
+        raise NotImplementedError
+    # End def #}}}
+
+    def _cmp(self, s, obj, val, target, options): #{{{
+        raise NotImplementedError
+    # End def #}}}
+
+    def _post_cmp(self, s, obj, val, target, options): #{{{
+        raise NotImplementedError
+    # End def #}}}
+
+    def _cmp_loop(self, s, obj, target, options): #{{{
+        raise NotImplementedError
+    # End def #}}}
+
+    def __transform__(self, obj): #{{{
+        return tuple(obj)
+    # End def #}}}
+
+    def __compare__(self, s, obj, **override): #{{{
+        options = dict(self._options)
+        options.update(override)
+        target = options.get('count', None)
+        if target is not None and target != MaxCount:
+            target = int(target)
+            if target < 0:
+                raise ValueError("count option must be >= 0: %i" %target)
+        return self._cmp_loop(s, obj, target, options)
+    # End def #}}}
+# End class #}}}
 
