13b44c6722666636b92bad69b7a17881e4a92e50
smokybobo <smokybobo@2d1e70a7-801d-0410-ab83-90da37f5c1c7>
2007-10-10 20:04:19 +0000

    Add commands to build c-extensions in setup.py

--- lib/pyssi/devel/setup.py
+++ lib/pyssi/devel/setup.py
@@ -4,15 +4,24 @@
 #
 # This module is part of the <WHAT-HAVE-YOU> project and is released under
 # the MIT License: http://www.opensource.org/licenses/mit-license.php
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Feature, Extension
+
+speedups = Feature(
+    "optional C speed-enhancement modules",
+    standard = True,
+    ext_modules = [
+        Extension("pyssi._speedups.core", ["src/pyssi/_speedups/core.pyx"]),
+        Extension("pyssi._speedups.util", ["src/pyssi/_speedups/util.pyx"]),
+    ]
+)
 
 setup(
-  name="PySSI",
-  version="0.0.1",
-  description="""Python signal slot implementation""",
-  author="Ariel De Ocampo",
-  author_email = 'arieldeocampo@gmail.com',
-  license = 'MIT',
+    name="PySSI",
+    version="0.0.1",
+    description="""Python signal slot implementation""",
+    author="Ariel De Ocampo",
+    author_email = 'arieldeocampo@gmail.com',
+    license = 'MIT',
 
 
 #  entry_points="""
@@ -22,7 +31,8 @@ setup(
 
 #  package_data={'testme': ['data/somefile.dat']},
 
-  packages=find_packages('src'),
-  package_dir={'': 'src'}
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    features={'speedups': speedups},
 )
 
