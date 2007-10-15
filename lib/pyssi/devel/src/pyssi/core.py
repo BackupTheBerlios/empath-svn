b053c89911c1eb8ae798497bc39bd556edc011e6
smokybobo <smokybobo@2d1e70a7-801d-0410-ab83-90da37f5c1c7>
2007-10-15 19:18:53 +0000

    Use c-extension by default and switch to pure python if import fails

--- lib/pyssi/devel/src/pyssi/core.py
+++ lib/pyssi/devel/src/pyssi/core.py
@@ -5,12 +5,28 @@
 # This module is part of the pyssi project and is released under
 # the MIT License: http://www.opensource.org/licenses/mit-license.php
 
-#from pyssi.util import property_, cref
+from pyssi.util import property_, cref
 try:
-    from pyssi._speedups.core import SignalType, BaseSignal, Signal
+    from pyssi._speedups.core import (SignalType, BaseSignal, 
+                                      next_before, 
+                                      next_streamin, next_stream, next_around,
+                                      next_replace, next_chooseyield, next_choosereturn, next_choice,
+                                      next_after, next_onreturn, Signal, 
+                                      LocalDecoratorSettings, GlobalDecoratorSettings,
+                                      GenericDecorator, DecoratorSignal,
+                                      signal, signal_factory)
 except ImportError:
-    from pyssi._core import SignalType, BaseSignal, Signal
+    from pyssi._core import (SignalType, BaseSignal, 
+                              next_before, 
+                              next_streamin, next_stream, next_around,
+                              next_replace, next_chooseyield, next_choosereturn, next_choice,
+                              next_after, next_onreturn, Signal, 
+                              LocalDecoratorSettings, GlobalDecoratorSettings,
+                              GenericDecorator, DecoratorSignal,
+                              signal, signal_factory)
 
-#from pyssi._speedups.core import SignalType, BaseSignal, Signal
-#from pyssi._core import SignalType, BaseSignal, Signal
+#from pyssi._speedups.core import (SignalType, BaseSignal, next_before, next_around, next_after, 
+#                         Signal, DecoratorSignal, signal)
+#from pyssi._core import (SignalType, BaseSignal, next_before, next_around, next_after, 
+#                         Signal, DecoratorSignal, signal)
 
