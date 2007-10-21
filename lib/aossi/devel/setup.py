2e8fa01b2965ff221585ed9af52ae4c584c9f7b3
smokybobo <smokybobo@2d1e70a7-801d-0410-ab83-90da37f5c1c7>
2007-10-21 19:08:40 +0000

    Auto merge

--- lib/aossi/devel/setup.py
+++ lib/aossi/devel/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aossi",
-    version="0.1.1-1",
+    version="0.2-1",
     packages=find_packages('src'),
     package_dir={'': 'src'},
 
