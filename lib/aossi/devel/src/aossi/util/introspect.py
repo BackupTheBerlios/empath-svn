e89736851989a95e094916f4600098d60b27fdc9
smokybobo <smokybobo@2d1e70a7-801d-0410-ab83-90da37f5c1c7>
2007-10-21 19:10:19 +0000

    Auto merge

--- lib/aossi/devel/src/aossi/util/introspect.py
+++ lib/aossi/devel/src/aossi/util/introspect.py
@@ -14,7 +14,8 @@ from types import (FunctionType as function, BuiltinFunctionType as bfunction,
         MethodType as method, BuiltinMethodType as bmethod, ClassType)
 
 __all__ = ('isclass', 'iscallable', 'isiterable', 'ismapping',
-            'isreadonly', 'isfunction', 'ismethod', 'isbuiltin')
+            'isreadonly', 'isfunction', 'ismethod', 'isbuiltin',
+            'isobjclass', 'mro')
 # ==================================================================================
 # Introspection functions
 # ==================================================================================
@@ -49,4 +50,22 @@ def isreadonly(obj, aname, attr): #{{{
     except:
         ret = True
     return ret
+# End def #}}}
+
+def isobjclass(obj): #{{{
+    return isinstance(obj, object) and isinstance(obj, type) and isclass(obj)
+# End def #}}}
+
+def mro(cls): #{{{
+    if isobjclass(cls):
+        return cls.mro()
+    def calc_mro(curcls): #{{{
+        if curcls is cls:
+            yield cls
+        for b in curcls.__bases__:
+            yield b
+            for c in calc_mro(b):
+                yield c
+    # End def #}}}
+    return [c for c in calc_mro(cls)]
 # End def #}}}
