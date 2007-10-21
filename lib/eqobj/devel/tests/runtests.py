0c449c868f808426a11b1eff03bacea2b63976f4
smokybobo <smokybobo@2d1e70a7-801d-0410-ab83-90da37f5c1c7>
2007-10-21 16:58:18 +0000

    Add nose-based test runner

--- lib/eqobj/devel/tests/runtests.py
+++ lib/eqobj/devel/tests/runtests.py
@@ -1,57 +1,119 @@
 # Module: template
 # File: template.py
-# Copyright (C) 2006 Ariel De Ocampo arieldeocampo@gmail.com
+# Copyright (C) 2007 Ariel De Ocampo arieldeocampo@gmail.com
 #
-# This module is part of the <WHAT-HAVE-YOU> project and is released under
+# This module is part of the doctestdir project and is released under
 # the MIT License: http://www.opensource.org/licenses/mit-license.php
 
-import unittest
-from smanstal.tests import addtest, mksuite
-
+# stdlib imports
+import os, os.path as op
 import sys
-from os.path import abspath, sep, isdir, join, dirname, basename
 
-basepackage = None
+# nose package imports
+from nose.core import TestProgram
+from nose.config import Config, all_config_files
+from nose.plugins.base import Plugin
+from nose.plugins.manager import DefaultPluginManager
+from nose.plugins.doctests import Doctest
 
-def set_private_packages(basedir): #{{{
-    basedir = abspath(basedir)
-    if not isdir(basedir):
-        raise OSError("No such directory: '%s'" %basedir)
-    paths = (['util', 'python'], ['tests', 'alltests'], ['pkg', None])
-    syspaths = [join(basedir, *p[:-1]) for p in paths]
-    oldpath = sys.path
-    sys.path = syspaths + sys.path
-    sysmod = sys.modules
-    for p in paths:
-        name = p[-1]
-        if not name:
-            continue
-        sysmod['/%s/_private/%s' %(basepackage, '/'.join(p))] = __import__(name)
-        sysmod.pop(name, None)
-    sys.path = oldpath
+def _add_ext_dot(el): #{{{
+    if not el.startswith('.'):
+        el = ''.join(['.', el])
+    return el
+# End def #}}}
+# =================================================
+# hasinit
+# =================================================
+def hasinit(dir, source_first=True): #{{{
+    source_first = (lambda seq: seq) if source_first else reversed
+    if isinstance(dir, basestring) and op.isdir(dir):
+        path1 = op.join(dir, '__init__.py')
+        path2, path3 = path1 + 'c', path1 + 'o'
+        for p in source_first((path1, path2, path3)):
+            if op.isfile(p):
+                return op.basename(p)
+        return ''
+#        return True in (op.isfile(p) for p in (path1, path2, path3))
+    raise TypeError("'%s' is not a directory path string" %str(dir))
 # End def #}}}
 
-# Create suite function for this package
-#suite = addtest(__file__)(mksuite(__file__))
+class DoctestDir(Doctest): #{{{
+    """
+    Activate doctestdir plugin to find and run doctests in non-package directories.
+    """
+    def options(self, parser, env=os.environ):
+        Plugin.options(self, parser, env)
+        parser.add_option('--doctestdir-tests', action='store_true',
+                          dest='doctest_tests',
+                          default=env.get('NOSE_DOCTEST_TESTS'),
+                          help="Also look for doctests in test modules. "
+                          "Note that classes, methods and functions should "
+                          "have either doctests or non-doctest tests, "
+                          "not both. [NOSE_DOCTEST_TESTS]")
+        parser.add_option('--doctestdir-extension', action="append",
+                          dest="doctestExtension",
+                          help="Also look for doctests in files with "
+                          "this extension [NOSE_DOCTEST_EXTENSION]")
+        parser.add_option('--doctestdir-recurse', action="store_true",
+                          default=False,
+                          dest="doctestdir_recurse",
+                          help="Recurse through any directories found")
+        # Set the default as a list, if given in env; otherwise
+        # an additional value set on the command line will cause
+        # an error.
+        env_setting = env.get('NOSE_DOCTEST_EXTENSION')
+        if env_setting is not None:
+            parser.set_defaults(doctestExtension=tolist(env_setting))
 
-def init(): #{{{
-    basedir = abspath(__file__)
-    basedir = join(sep, *dirname(basedir).split(sep)[1:-1])
-    global basepackage
-    if not basepackage:
-        basepackage = basename(basedir)
-    set_private_packages(basedir)
+    def configure(self, options, config):
+        super(DoctestDir, self).configure(options, config)
+        self.extension = [_add_ext_dot(el) for el in self.extension]
+        abspath = op.abspath
+        where = options.where
+        if where == None:
+            where = [os.getcwd()]
+        self.where = set(abspath(p) for p in where)
+        self.doctestdir_recurse = options.doctestdir_recurse
+
+    def loadTestsFromDir(self, path): #{{{
+        recurse = self.doctestdir_recurse
+        loadfile = self.loadTestsFromFile
+        pjoin = op.join
+        where = self.where
+        if not hasinit(path):
+            for curdir, subdir, files in os.walk(path):
+                if curdir not in where:
+                    for f in files:
+                        i = loadfile(pjoin(curdir, f))
+                        if i != None:
+                            for res in i:
+                                if res:
+                                    yield res
+                if not recurse:
+                    break
+    # End def #}}}
+
+# End class #}}}
+
+class NosePluginManager(DefaultPluginManager): #{{{
+    def loadPlugins(self): #{{{
+        super(NosePluginManager, self).loadPlugins()
+        self.addPlugin(DoctestDir())
+    # End def #}}}
+# End class #}}}
+
+def run_nose(): #{{{
+    argv = list(sys.argv)
+    execpath = op.realpath(argv[0])
+    testdir = op.abspath(op.dirname(execpath))
+    if len(argv) == 1:
+        argv.extend(['doc', 'unit'])
+    os.chdir(testdir)
+    env = os.environ
+    cfg_files = all_config_files()
+    config = Config(env=env, files=cfg_files, plugins=NosePluginManager())
+    TestProgram(argv=argv, env=env, config=config)
 # End def #}}}
 
 if __name__ == '__main__':
-    # Set up environment
-    init()
-    alltests = __import__('/%s/_private/tests/alltests' %basepackage)
-    python = __import__('/%s/_private/util/python' %basepackage)
-    app_version = __import__('/%s/_private/util/python.version' %basepackage)
-
-    alltests = alltests.__file__
-    suite = addtest(alltests)(mksuite(alltests))
-
-    # Run the tests
-    unittest.TextTestRunner(verbosity=2).run(suite())
+    run_nose()
